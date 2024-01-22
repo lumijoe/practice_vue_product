3c71d00b0fbfd053a910c524abcf4cd9950c6429
lumijoe <spacedesign63@gmail.com>
2024-01-22 13:50:52 +0900

    update

--- js/hello.js
+++ js/hello.js
@@ -8,7 +8,8 @@ let app = new Vue({
         url: 'https://v2.ja.vuejs.org/v2/guide/installation',
         url2: 'https://v2.ja.vuejs.org/v2/guide/installation',
         message4: '省略形で記述：Vue公式ページ',
-        flag: false,
+        flag: false
+        
 
     }
 });
@@ -16,13 +17,32 @@ let app = new Vue({
 let appTwo = new Vue({
     el: '#appTwo',
     data: {
-        email: 'Y-suzuki@example.com'
+        email1: 'Y-suzuki@example.com',
+        email2: 'S-tAKESHI@example.com',
+        caseComputed: '引数不可のプロパティ、引数可のメソッドの違い',
+        current: new Date().toLocaleString()
     },
 
-    // 処理：算出を取得
+    // 処理：算出プロパティの場合（取得用途、引数はNG）
     computed: {
         localEmail: function() {
-            return this.email.split('@')[0].toLowerCase();
+            return this.email1.split('@')[0].toLowerCase();
+        },
+        randomComputed: function() {
+            return Math.random();
+        }
+    },
+
+    // 処理：メソッドの場合（HTML、js側共に()引数の使用が可能）
+    methods: {
+        localEmail2: function() {
+            return this.email2.split('@')[0].toLowerCase();
+        }, 
+        onclick: function() {
+            this.current = new Date().toLocaleString();
+        },
+        randomMethods: function() {
+            return Math.random();
         }
     }
 });
