c518e093de858332c81e91580f718c1fa2c685f6
lumijoe <spacedesign63@gmail.com>
2024-01-22 12:07:18 +0900

    test

---
+++ js/hello.js
@@ -0,0 +1,10 @@
+// hello.js
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'おはよう！',
+        message2: 'こんにちわ！',
+        message3: 'Vue公式ページ（本番ver.インストール）',
+        url: 'https://v2.ja.vuejs.org/v2/guide/installation'
+    }
+});
